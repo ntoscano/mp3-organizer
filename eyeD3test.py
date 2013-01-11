20358ed6fe25d1b7f1d2a17c625b4c3a63c47996
Nicolas Toscano <nicolas.toscano@gmail.com>
2013-01-11 13:34:39 -0800

    fixed bugs that came up during application

--- eyeD3test.py
+++ eyeD3test.py
@@ -3,7 +3,7 @@ import eyed3 #imports eyed3 functions
 import errno
 import shutil
 
-root_folder = '/Users/ntoscano/Desktop/mp3-organizer'
+root_folder = '/Volumes/Other Stuff/music/music'
 
 files = os.listdir(root_folder) #lists all files in specified directory
 
@@ -13,22 +13,45 @@ for file_name in files:
         
         abs_location = '%s/%s' % (root_folder, file_name)
         
-        song_info = eyed3.load(abs_location) #loads each file into eyed3 and assignes the return value to song_info
+        try:
+            song_info = eyed3.load(abs_location) #loads each file into eyed3 and assignes the return value to song_info
+        except IOError:
+            pass
+        if song_info.tag is None:
+            print 'Skipping %s' % abs_location
+            continue
         if song_info is None:
-            print 'Skippig %s' % abs_location
+            print 'Skipping %s' % abs_location
+            continue
+            print 'Skipping %s' % abs_location
             continue
         try:
-            os.mkdir(os.path.expanduser('~/Desktop/mp3-organizer/%s' % song_info.tag.artist))
+            os.mkdir(os.path.expanduser('/Volumes/Other Stuff/music/music/%s' % song_info.tag.artist))
         except OSError as e:
             if e.errno!= errno.EEXIST:
                 raise
+        except UnicodeDecodeError:
+            pass
         try:
-            os.mkdir(os.path.expanduser('~/Desktop/mp3-organizer/%s/%s' % (song_info.tag.artist, song_info.tag.album)))
+            os.mkdir(os.path.expanduser('/Volumes/Other Stuff/music/music/%s/%s' % (song_info.tag.artist, song_info.tag.album)))
         except OSError as e:
             if e.errno!= errno.EEXIST:
                 raise
+        except OSError:
+                continue
         print song_info
         print song_info.tag.artist
-        shutil.move('%s' % file_name, '%s/%s' % (song_info.tag.artist, song_info.tag.album))
+        try :
+            shutil.move('%s' % file_name, '%s/%s' % (song_info.tag.artist, song_info.tag.album))
+        except UnicodeDecodeError:
+            pass
+        except shutil.Error:
+            try:
+                os.renames('%s' % file_name, '%s_%s' % (file_name, song_info.tag.artist))
+            except OSError:
+                pass
     else:
         pass
+
+#improvements
+    #have this work on other computers
